44c474f886be3309bc3079476a09c6b8499b190a
김선아 <127083014+kxmseona@users.noreply.github.com>
2023-04-10 13:52:40 +0900

    Add files via upload

---
+++ main/main.js
@@ -0,0 +1,9 @@
+// login 버튼을 클릭하면 login.html로 이동
+document.getElementById("login-btn").addEventListener("click", function(){
+    window.location.href = "login.html";
+});
+
+// signup 버튼을 클릭하면 signup.html로 이동
+document.getElementById("signup-btn").addEventListener("click", function(){
+    window.location.href = "signup.html";
+});
